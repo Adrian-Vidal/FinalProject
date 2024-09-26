7fc4d5ab93ec375c51ff5f3710a107502a6d3da8
Adrian <adrian.vidal9589@gmail.com>
2024-09-26 13:02:16 -0600

    Create Report Tag Entity

---
+++ NeighborNetJPA/src/main/java/com/skilldistillery/communityevents/entities/ReportTag.java
@@ -0,0 +1,66 @@
+package com.skilldistillery.communityevents.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ReportTag {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="tag_name")
+	private String tagName;
+
+	public ReportTag() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReportTag other = (ReportTag) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ReportTag [id=").append(id).append(", tagName=").append(tagName).append("]");
+		return builder.toString();
+	}
+	
+	
+
+}
