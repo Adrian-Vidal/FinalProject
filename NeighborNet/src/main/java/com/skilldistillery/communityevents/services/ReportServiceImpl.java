121f2b61a0720e6c7b1fff5efaf5e8bf3348f22f
“Justin” <jdenton.bills@gmail.com>
2024-09-26 18:09:08 -0500

    Added 1 insert into report for testing

--- NeighborNet/src/main/java/com/skilldistillery/communityevents/services/ReportServiceImpl.java
+++ NeighborNet/src/main/java/com/skilldistillery/communityevents/services/ReportServiceImpl.java
@@ -33,9 +33,9 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public Report create(String username, Report report) {
 		User user = userRepo.findByUsername(username);
-		if (user != null) {
-			// report.set
-		}
+		// if (user != null) {
+			//report.setU
+		// }
 		return null;
 	}
 
