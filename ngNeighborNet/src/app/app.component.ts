651e212c284bc7dd91b9e708118a81787867615d
Adrian <adrian.vidal9589@gmail.com>
2024-09-27 10:37:18 -0600

    Add and route navigation bar component

--- ngNeighborNet/src/app/app.component.ts
+++ ngNeighborNet/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { NavigationbarComponent } from "./components/navigationbar/navigationbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
