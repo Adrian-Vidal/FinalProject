99b07719c44771858c8fb517feb07c3e8b6b528c
Adrian <adrian.vidal9589@gmail.com>
2024-09-27 13:12:23 -0600

    Sync merge

--- ngNeighborNet/src/app/components/login/login.component.spec.ts
+++ ngNeighborNet/src/app/components/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { LandingComponent } from '../landing/landing.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
--- ngNeighborNet/src/app/components/login/login.component.ts
+++ ngNeighborNet/src/app/components/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginUser: User = new User();
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ){}
+
+  login(user: User){
+    console.log(user);
+  }
 
 }
