a39e81be03bdf60fd6e26bc5faa3efb332bd02c8
“Justin” <jdenton.bills@gmail.com>
2024-09-28 13:35:00 -0500

    Functional login/logout behavior in navbar

--- ngNeighborNet/src/app/components/login/login.component.ts
+++ ngNeighborNet/src/app/components/login/login.component.ts
@@ -3,15 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { User } from '../../models/user';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   loginUser: User = new User();
 
   constructor(
@@ -27,6 +32,7 @@ export class LoginComponent {
           error: (problem) => {
             console.error('LoginComponent.login(): Error logging in user: ');
             console.error(problem);
+            this.router.navigateByUrl('/login');
           }
         })
       }
