253aa07bc219ea2d35f36c8d322d226e52f90874
“Justin” <jdenton.bills@gmail.com>
2024-09-30 11:33:51 -0500

    Stub profile page

--- ngNeighborNet/src/app/app.routes.ts
+++ ngNeighborNet/src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { NavigationbarComponent } from './components/navigationbar/navigationbar.component';
 import { MapReportsComponent } from './components/map-reports/map-reports.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'landing', component: LandingComponent},
+  {path: 'map', component: MapReportsComponent},
+  {path: 'profile', component: ProfileComponent}
 
-  {path: 'map', component: MapReportsComponent}
 
 ];
---
+++ ngNeighborNet/src/app/components/profile/profile.component.ts
@@ -0,0 +1,60 @@
+import { AuthService } from './../../services/auth.service';
+import { Report } from './../../models/report';
+import { ReportService } from './../../services/report.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Address } from '../../models/address';
+import { Reportcategory } from '../../models/reportcategory';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent implements OnInit {
+
+  reports: Report [] = [];
+  newReport: Report = new Report();
+  loggedInUser: User | null = null;
+
+constructor (
+  private AuthService: AuthService,
+  private reportService: ReportService
+){}
+
+ngOnInit(): void {
+  this.reload();
+}
+
+reload() {
+  this.reportService.index().subscribe({
+    next: (reports) => {
+      this.reports = reports;
+    },
+    error: (err) => {
+      console.error('Error loading reports: ', err);
+    }
+  });
+}
+
+addReport(report: Report): void {
+  this.reportService.create(report).subscribe({
+    next: (createdReport) => {
+      this.reload();
+      this.newReport = new Report();
+      // this.loadForeign();
+    },
+    error: (err) => {
+      console.error('landing.component - addReport(): Error adding report', err);
+    }
+  });
+}
+
+}
