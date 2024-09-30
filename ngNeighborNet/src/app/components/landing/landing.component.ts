fcd91722b36680b54d3c492779eb2c6bb470ede2
Adrian <adrian.vidal9589@gmail.com>
2024-09-30 17:42:03 -0600

    Implement edit report button. Add update form to landing page. Update report.service.ts, landing.component.html, landing.component.ts.

--- ngNeighborNet/src/app/components/landing/landing.component.ts
+++ ngNeighborNet/src/app/components/landing/landing.component.ts
@@ -1,4 +1,3 @@
-import { User } from './../../models/user';
 import { Report } from './../../models/report';
 import { ReportService } from './../../services/report.service';
 import { CommonModule } from '@angular/common';
@@ -23,6 +22,63 @@ export class LandingComponent implements OnInit{
 
   reports: Report [] = [];
   newReport: Report = new Report();
+  showUpdateForm: Report | null = null;
+  editReport: Report = {
+    name: '',
+    description: '',
+    imageUrl: '',
+    address: {
+      name: '',
+      street: '',
+      city: '',
+      state: '',
+      postalCode: 0,
+      country: '',
+      id: 0,
+      enabled: false
+    },
+    reportCategory: {
+      name: '',
+      id: 0,
+      description: '',
+      imageUrl: ''
+    },
+    id: 0,
+    createDate: '',
+    modifiedDate: '',
+    eventDate: '',
+    eventDateEnd: '',
+    resolved: false,
+    enabled: false,
+    user: {
+      id: 0,
+      email: '',
+      username: '',
+      password: '',
+      enabled: false,
+      role: '',
+      firstName: '',
+      lastName: '',
+      imageUrl: '',
+      address: {
+        id: 0,
+        name: '',
+        street: '',
+        city: '',
+        state: '',
+        postalCode: 0,
+        country: '',
+        enabled: false
+      }
+    },
+    severity: {
+      id: 0,
+      name: '',
+      level: 0,
+      imageUrl: '',
+    }
+  };
+
 
 constructor (
   private reportService: ReportService,
@@ -59,6 +115,42 @@ addReport(report: Report): void {
       console.error('landing.component - addReport(): Error adding report', err);
     }
   });
+}
+
+
+displayLandingPage(): void {
+  this.showUpdateForm = null;
+}
+
+
+displayUpdateForm(report: Report): void {
+  this.showUpdateForm = report;
+  this.setEditEvent;
+}
+
+updateReport(): void{
+  if (this.editReport) {
+    this.reportService.update(this.editReport).subscribe({
+      next: () => {
+        this.reload();
+        this.editReport;
+
+      },
+
+    });
+  }
+}
+
+
+
+setEditEvent(): void {
+  if (this.showUpdateForm) {
+    this.editReport = Object.assign({}, this.showUpdateForm);
+  }
+}
+
+cancelEdit(): void {
+  this.editReport;
 }
 
 
