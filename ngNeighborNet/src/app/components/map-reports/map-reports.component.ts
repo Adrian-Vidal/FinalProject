b8ff1781b51ba881ca3eafdf9dbcb00ab02f0295
“Justin” <jdenton.bills@gmail.com>
2024-10-04 11:57:12 -0500

    Update map with testing

--- ngNeighborNet/src/app/components/map-reports/map-reports.component.ts
+++ ngNeighborNet/src/app/components/map-reports/map-reports.component.ts
@@ -1,76 +1,130 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleMap, GoogleMapsModule, MapGeocoder } from '@angular/google-maps';
-import { User } from '../../models/user';
-import { Address } from '../../models/address';
-import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MapReportService } from '../../services/map-report.service';
+import { ReportService } from '../../services/report.service';
+import { AuthService } from '../../services/auth.service';
+import { Report } from './../../models/report';
+import {
+  GoogleMap,
+  MapInfoWindow,
+  MapMarker,
+  MapAdvancedMarker,
+} from '@angular/google-maps';
 
 @Component({
   selector: 'app-map-reports',
   standalone: true,
   imports: [
+    FormsModule,
+    CommonModule,
     GoogleMap,
-    GoogleMapsModule,
-    CommonModule
+    MapMarker,
+    MapInfoWindow,
+    MapMarker,
+    MapAdvancedMarker,
   ],
   templateUrl: './map-reports.component.html',
-  styleUrl: './map-reports.component.css'
+  styleUrl: './map-reports.component.css',
 })
+
 export class MapReportsComponent implements OnInit {
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
+  reports: Report[] = [];
 
-  user: User | null = null;
-  addr: Address | null = null;
-
-  center: google.maps.LatLngLiteral | null = null;
-  zoom = 4;
-  display: google.maps.LatLngLiteral | null = null;
+  center: google.maps.LatLngLiteral = { lat: 36.637, lng: -93.254 };
+  zoom = 12;
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  markerPositions: { position: google.maps.LatLngLiteral; report: Report }[] =
+    [];
+  newReport: Report = new Report();
+  infoWindowContent: string = '';
+  selected: Report | null = null;
 
   constructor(
-    private authService: AuthService,
-    private geocoder: MapGeocoder
-  ){}
+    private mapReportService: MapReportService,
+    private reportService: ReportService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.loadUserToMap();
-
+    this.reload();
   }
 
-  loadUserToMap(): void {
-    this.authService.getLoggedInUser().subscribe({
-      next: (loggedInUser) => {
-        this.user = loggedInUser;
-        this.getUserLocation();
+  reload() {
+    this.reportService.index().subscribe({
+      next: (recievedReports) => {
+        this.reports = recievedReports; //Loads reports to this.reports
+        this.reloadMarkerPositions(); //Loads markerPositions with
+        // this.mapMarker(this.markerPositions);   ////  STEP 1
       },
-      error: (problem) => {
-        console.error('MapReportsComponent.loadUserToMap(): Error loading user with map');
-        console.error(problem);
-      }
-    })
-  }
-
-  getUserLocation() {
-    if (this.user){
-      let a = this.user.address;
-      let addr = `${a.street}, ${a.city}, ${a.state}, ${a.postalCode}`;
-    this.geocoder.geocode({
-      address: addr
-    }).subscribe(({results}) => {
-      console.log(results);
-      let loc = results[0].geometry.location;
-      this.center = { lat: loc.lat(), lng: loc.lng() }
+      error: (err) => {
+        console.error('Error loading reports: ', err);
+      },
     });
   }
+  reloadMarkerPositions() {
+    for (let report of this.reports) {
+      console.log(report.address);
+      this.mapReportService.getLongAndLat(report.address).subscribe({
+        next: (geoResponse) => {
+          const position = {
+            lat: geoResponse.results[0].geometry.location.lat(),
+            lng: geoResponse.results[0].geometry.location.lng(),
+          };
+          this.markerPositions.push({ position, report });
+          // this.markerPositions.push({lat: geoResponse.results[0].geometry.location.lat(), lng: geoResponse.results[0].geometry.location.lng()});
+        },
+      });
+    }
   }
-  // center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
-  // zoom = 4;
-  // display: google.maps.LatLngLiteral;
 
-  // moveMap(event: google.maps.MapMouseEvent) {
-  //   this.center = (event.latLng.toJSON());
+  //-------------------map marker info window------------------------
+
+  openInfoWindow(marker: MapMarker, report: Report) {
+    console.log('map marker clicked');
+    if (this.infoWindow) {
+      this.infoWindow.open(marker);
+      this.infoWindowContent = report.name;
+      console.log(this.infoWindow.getContent);
+    }
+    this.selected = report;
+    console.log('hello???');
+    console.log(this.selected);
+  }
+
+  ////-------------------Display and Open------------------------
+  displayAndOpen() {}
+
+  //132 Cape Cod Dr, Branson, MO 65616
+  //  testAddress: Address = new Address();
+
+  // //  makeTestAddress(){
+  // //    this.testAddress.street="132 Cape Cod Dr";
+  // //    this.testAddress.city = "Branson";
+  // //    this.testAddress.state = "MO";
+  // //  }
+  //  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  // ------------------------Dont think this is in use anymore-------------v------------
+  // showLongLat(address : Address){
+  //   this.mapReportService.getLongAndLat(address).subscribe({
+  //     next: (geoResponse) => {
+  //       console.log(geoResponse.results[0].geometry.location.lat());
+
+  //       this.markerPositions.push({lat: geoResponse.results[0].geometry.location.lat(), lng: geoResponse.results[0].geometry.location.lng()});
+
+  //       console.log(geoResponse.results[0].geometry.location.lng());
+  //     }
+  //   });
   // }
 
-  // move(event: google.maps.MapMouseEvent) {
-  //   this.display = event.latLng.toJSON();
-  // }
+  //-------------------Add a marker on the map doesnt work i think------------------------
+  // mapMarker(markerPosition: google.maps.LatLngLiteral[]){
 
+  //   for (let marker of markerPosition){
+
+  //     this.markerPositions = this.mapReportService.addMarker({lat: marker.lat, lng: marker.lng});
+  //   }
+  // }
 }
